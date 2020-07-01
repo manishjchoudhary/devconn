5728f9340599483be287aa1387e5f0837bfa39ef
red <manishchoudhary0870@gmail.com>
2020-07-01 15:30:06 +0530

    react layout comp added

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Navbar from './components/layouts/Navbar';
+import Footer from './components/layouts/Footer';
+import Landing from './components/layouts/Landing';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>React boilerplate</h1>
+      <Navbar />
+      <Landing />
+      <Footer />
     </div>
   );
 }
