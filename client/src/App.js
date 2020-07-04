0e4e2d47ee3d6c952e938e1cddaae9bafe0ff225
red <manishchoudhary0870@gmail.com>
2020-07-05 01:32:10 +0530

    router setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import Navbar from './components/layouts/Navbar';
 import Footer from './components/layouts/Footer';
 import Landing from './components/layouts/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <Landing />
-      <Footer />
-    </div>
-  );
+class App extends Component {
+	render() {
+		return (
+			<Router>
+				<div className="App">
+					<Navbar />
+					<Route exact path="/" component={Landing} />
+					<div className="container">
+						<Route exact path="/register" component={Register} />
+						<Route exact path="/login" component={Login} />
+					</div>
+					<Footer />
+				</div>
+			</Router>
+		);
+	}
 }
 
 export default App;
